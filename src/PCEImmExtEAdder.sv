2c0fbb2eb5d813b44a06992ee4bd351f2630e664
Ajeeth93 <ajeethsoundararajan@gmail.com>
2026-08-15 10:57:18 -0400

    Refactored RISC_V_Pipeline_PCEImmExtEAdder module

--- src/PCEImmExtEAdder.sv
+++ src/PCEImmExtEAdder.sv
@@ -1,6 +1,9 @@
-module RISC_V_Pipeline_PCEImmExtEAdder(PCE, ImmExtE, PCTargetE);
-input logic [31:0] PCE;
-input logic [31:0] ImmExtE;
-output logic [31:0] PCTargetE;
-assign PCTargetE = PCE + ImmExtE;
+module RISC_V_Pipeline_PCEImmExtEAdder (
+    input  logic [31:0] PCE,
+    input  logic [31:0] ImmExtE,
+    output logic [31:0] PCTargetE
+);
+
+    assign PCTargetE = PCE + ImmExtE;
+
 endmodule
