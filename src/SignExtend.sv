45473e4853acb6f656a4082d0f0a534307f40b6b
Ajeeth93 <ajeethsoundararajan@gmail.com>
2026-08-15 03:07:19 -0400

    Added SignExtend module to use logic types
    Refactored the SignExtend module to use logic types and always_comb for combinational logic.

--- src/SignExtend.sv
+++ src/SignExtend.sv
@@ -1,15 +1,17 @@
-module RISC_V_Pipeline_SignExtend(InstrD, ImmExtD, ImmSrcD);
-input [31:0] InstrD;
-input [1:0] ImmSrcD;
-output reg [31:0] ImmExtD;
-always @(*)
-begin
-case(ImmSrcD)
-2'b00: ImmExtD = {{20{InstrD[31]}}, InstrD[31:20]};
-2'b01: ImmExtD = {{20{InstrD[31]}}, InstrD[31:25], InstrD[11:7]};
-2'b10: ImmExtD = {{19{InstrD[31]}}, InstrD[31], InstrD[7],InstrD[30:25], InstrD[11:8], 1'b0};
-2'b11: ImmExtD = {{11{InstrD[31]}}, InstrD[31], InstrD[19:12], InstrD[20], InstrD[30:21], 1'b0};
-default: ImmExtD = 32'b0;
-endcase
-end
+module RISC_V_Pipeline_SignExtend (
+    input  logic [31:0] InstrD,
+    input  logic [1:0]  ImmSrcD,
+    output logic [31:0] ImmExtD
+);
+
+    always_comb begin
+        case (ImmSrcD)
+            2'b00:   ImmExtD = {{20{InstrD[31]}}, InstrD[31:20]};                                   // I-Type
+            2'b01:   ImmExtD = {{20{InstrD[31]}}, InstrD[31:25], InstrD[11:7]};                     // S-Type
+            2'b10:   ImmExtD = {{19{InstrD[31]}}, InstrD[31], InstrD[7], InstrD[30:25], InstrD[11:8], 1'b0};  // B-Type
+            2'b11:   ImmExtD = {{11{InstrD[31]}}, InstrD[31], InstrD[19:12], InstrD[20], InstrD[30:21], 1'b0}; // J-Type
+            default: ImmExtD = 32'b0;
+        endcase
+    end
+
 endmodule
